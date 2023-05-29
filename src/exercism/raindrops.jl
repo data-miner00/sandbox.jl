c40b0ccefb2cdaf897c033319a4d7fc9cd844f4b
data-miner00 <data-miner00@users.noreply.github.com>
2023-05-29 21:59:24 +0800

    docs: Add community solutions

--- src/exercism/raindrops.jl
+++ src/exercism/raindrops.jl
@@ -10,3 +10,20 @@ function raindrops(number::Int)
         (div3 ? "Pling" : "") * (div5 ? "Plang" : "") * (div7 ? "Plong" : "")
     end
 end
+
+
+# Author: taigua
+# Link: https://exercism.org/tracks/julia/exercises/raindrops/solutions/taigua
+function raindrops_v2(number::Int)
+    rs = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+    s = map(x -> x[2], filter(x -> number % x[1] == 0, rs)) |> join
+    length(s) == 0 ? string(number) : s
+end
+
+
+# Author: justinluther502
+# Link:
+function raindrops_v3(number::Real)
+    factors = [number % 3 == 0, number % 5 == 0, number % 7 == 0]
+    any(factors) ? string(("Pling", "Plang", "Plong")[factors]...) : string(n)
+end
